e73613ecc4bab6c07ca4bd860f17b0821cfcd259
Balusu Aditya <aditya@bubblewrapp.com>
2015-01-22 17:06:59 +0530

    Setting mangle false for uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,11 @@ module.exports = function (grunt) {
 
   // Define the configuration for all the tasks
   grunt.initConfig({
-
+    uglify:{
+      options:{
+        mangle:false
+      }
+    },
     // Project settings
     yeoman: {
       // configurable paths
