c5ac958fae7e2cfab3d765ca0b07fcbc8e7145d9
Balusu Aditya <aditya@bubblewrapp.com>
2015-01-24 17:48:18 +0530

    Adding a 'signup-feedback-okay' workflow.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,24 +3,24 @@
 angular.module('geekValetLanding')
   .controller('MainCtrl',['$scope','Api','ngDialog',function ($scope,api,ngDialog) {
        
-       $scope.user = {};
-       $scope.submit = function(){
-         var postdata = {email:$scope.user.email,feedback:$scope.user.feedback}
-         api.signUp.post(postdata);
-       }
-
-       $scope.openDialog = function(){
-            var postdata = {email:$scope.user.email}
-            api.signUp.post(postdata).then(function(response){
-              ngDialog.open({
-                template:'views/feedback.html',
-                scope:$scope,
-                showClose:false,
-                className: 'ngdialog-theme-default'
-              })      
-            });
-            
-        }            
-        
-
-  }]);
+    $scope.user = {};
+    $scope.submit = function(){
+        var postdata = {email:$scope.user.email,feedback:$scope.user.feedback}
+        api.signUp.post(postdata).then(function(response){
+          $scope.submitted = true;
+          $scope.submittedFeedback = true;
+        });
+    }
+     $scope.openDialog = function(){
+        var postdata = {email:$scope.user.email}
+        $scope.submittedFeedback = false;
+        api.signUp.post(postdata).then(function(response){
+          ngDialog.open({
+            template:'views/feedback.html',
+            scope:$scope,
+            showClose:false,
+            className: 'ngdialog-theme-default'
+          })      
+        });
+    }            
+}]);
