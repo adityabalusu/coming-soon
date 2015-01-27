17c53d94143c395f5d045d295bb04796c5619138
Balusu Aditya <aditya@bubblewrapp.com>
2015-01-27 14:51:36 +0530

    Some content changes to the dialogs.

--- app/scripts/services/helpText.js
+++ app/scripts/services/helpText.js
@@ -4,7 +4,8 @@
   helpText = {
     staticText: {
       darkContainerText: 'We know it is not cool. People like you should not be worrying about things like mowing. A supersolution to handle all your household services is on its way. And you won\'t see it coming.',
-      thankyoumsg:'We\'ll let you know <br> It would be great if you tell us which services you would like to book online <br>You don\'t have to, though.'
+      thankyoumsg:'We love you. Also can you tell us which services you would like to book online? <br>You don\'t have to, though.',
+      signedUpmsg:'Thanks for being a bro. We\'ll let you know when we take off.'
     }
   };
   angular.module('geekValetLanding').constant('helpText', helpText);
