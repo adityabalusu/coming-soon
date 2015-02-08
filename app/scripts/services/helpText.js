834c393a0b3e9b1f34946c78af113d024ce48346
Balusu Aditya <aditya@bubblewrapp.com>
2015-02-09 01:50:41 +0530

    Content integration.

--- app/scripts/services/helpText.js
+++ app/scripts/services/helpText.js
@@ -3,7 +3,7 @@
   var helpText;
   helpText = {
     staticText: {
-      darkContainerText: 'We know it is not cool. People like you should not be worrying about things like mowing. A supersolution to handle all your household services is on its way. And you won\'t see it coming.',
+      darkContainerText: 'Fed up of your weekly laundry or searching for the key maker? <br> Ever struggled to find a plumber, electrician or a carpenter?<br>Wish you could get an enriching facial at home? ',
       thankyoumsg:'We love you. Also can you tell us which services you would like to book online? <br>You don\'t have to, though.',
       signedUpmsg:'Thanks for being a bro. We\'ll let you know when we take off.'
     }
