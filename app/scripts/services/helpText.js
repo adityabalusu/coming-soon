7b20aa3bb2fc34bcafd7e539da43b335184814c3
Balusu Aditya <balusu.aditya@gmail.com>
2015-02-11 02:19:36 +0530

    Adding service tiles to the dialog.

--- app/scripts/services/helpText.js
+++ app/scripts/services/helpText.js
@@ -4,7 +4,7 @@
   helpText = {
     staticText: {
       darkContainerText: 'Fed up of your weekly laundry or searching for the key maker? <br> Ever struggled to find a plumber, electrician or a carpenter?<br>Wish you could get an enriching facial at home?<br>We\'ve got you covered.  ',
-      thankyoumsg:'Thanks for Signing up. Below services will be at your fingertips soon!',
+      thankyoumsg:'Thanks for signing up. These services will be soon at your fingertips.',
       signedUpmsg:'Thanks for being a bro. We\'ll let you know when we take off.'
     }
   };
