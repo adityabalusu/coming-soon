3ec70e0c0fbab726334eca088c1442a590f4d963
Balusu Aditya <balusu.aditya@gmail.com>
2015-04-12 22:55:38 +0530

    fixing geekvaletLadingApp issue with ratingsCtrl

--- app/scripts/controllers/rating.js
+++ app/scripts/controllers/rating.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('geekValetLandingApp')
+angular.module('geekValetLanding')
   .controller('RatingCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
