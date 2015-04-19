1a861cc8a5fa977e2cc190f7985c12c8830f1dbb
Balusu Aditya <balusu.aditya@gmail.com>
2015-04-20 01:49:20 +0530

    adding dev api-connect proxies to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -64,13 +64,45 @@ module.exports = function (grunt) {
         hostname: 'localhost',
         livereload: 35729
       },
+      proxies: [{
+          context: "/api/search/",
+          host: "localhost",
+          port: 8889,
+          https: false,
+          changeOrigin: true,
+          xforward: false
+        },{
+          context: "/api/",
+          host: "localhost",
+          port: 8888,
+          https: false,
+          changeOrigin: true,
+          xforward: false
+        }],
       livereload: {
         options: {
           open: true,
           base: [
             '.tmp',
             '<%= yeoman.app %>'
-          ]
+          ],
+          middleware: function (connect, options) {
+            var middlewares = [];
+               
+            if (!Array.isArray(options.base)) {
+              options.base = [options.base];
+            }
+               
+            // Setup the proxy
+            middlewares.push(require('grunt-connect-proxy/lib/utils').proxyRequest);
+
+            // Serve static files
+            options.base.forEach(function(base) {
+              middlewares.push(connect.static(base));
+            });
+
+            return middlewares;
+          }
         }
       },
       test: {
@@ -362,6 +394,7 @@ module.exports = function (grunt) {
       'clean:server',
       'bower-install',
       'concurrent:server',
+      'configureProxies:server',
       'autoprefixer',
       'connect:livereload',
       'watch'
